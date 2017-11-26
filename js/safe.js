e3e57c132102b7a624c3070b6476819047737102
Aditya Pratama <muhammadfanyadityapratama@gmail.com>
2017-11-26 15:34:16 +0700

    update post

--- js/safe.js
+++ js/safe.js
@@ -21,13 +21,13 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*7)+ 1);
 var xxx=null;
-if(x=="1"){xxx="tips-to-increase-benefits-from-insurance/"}
-if(x=="2"){xxx="health-insurance-benefits-that-serve-your-needs/"}
-if(x=="3"){xxx="home-insurance-is-very-important-for-a-peaceful-life/"}
-if(x=="4"){xxx="one-must-choose-the-best-auto-insurance-company/"}
-if(x=="5"){xxx="how-get-free-insurance-quotes/"}
-if(x=="6"){xxx="are-you-a-higher-risk-health-insurance-applicant/"}
-if(x=="7"){xxx="who-else-wants-low-costs-auto-insurance/"}a_to_ve[i].href="http://de4ce.gq/"+xxx+"?u="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="5-ways-to-increase-benefits-from-insurance/"}
+if(x=="2"){xxx="health-insurance-benefits-that-help-your-life/"}
+if(x=="3"){xxx="reasons-why-home-insurance-is-important-for-your-life/"}
+if(x=="4"){xxx="you-must-find-the-best-auto-insurance-company/"}
+if(x=="5"){xxx="how-to-get-the-best-free-insurance-quotes/"}
+if(x=="6"){xxx="factors-affect-your-health-insurance-costs/"}
+if(x=="7"){xxx="tips-to-get-low-costs-auto-insurance/"}a_to_ve[i].href="http://de4ce.gq/"+xxx+"?u="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
